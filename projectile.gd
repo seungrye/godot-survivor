7e05abe2b587baf968ad788c55f2a8c05fd93017
안승례 <seungrye@gmail.com>
2026-03-18 23:23:54 +0900

    add projectile related code

---
+++ projectile.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var direction: Vector2 = Vector2.RIGHT
+var speed: float = 200
+var damage: float = 1
+
+func _physics_process(delta: float) -> void:
+	self.position += direction * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	# call take_damage function if the inteacting body has one
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free() # free the projectile when it leaves the screen
